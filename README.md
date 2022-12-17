66cc8e448507244ef7c07602aa9a158d8fd92a78
Nikos Patsiatzis <n.patsiatzis@gmail.com>
2022-12-17 14:54:27 +0200

    added a CoCoTB-test simulation flow to run the tests across a range of values for generic parameters

--- README.md
+++ README.md
@@ -5,4 +5,7 @@
 - uses a toggle pulse synchronizer to transmit a pulse across clock domains
 - pulses controls the sampling of the data bus on the receiving (sink) domain
 - CoCoTB testbench for functional verification
+    - $ make
+- CoCoTB-test unit testing to exercise the CoCoTB tests across a range of values for the generic parameters
+    - $  SIM=ghdl pytest -n auto -o log_cli=True --junitxml=test-results.xml --cocotbxml=test-cocotb.xml
 
---
+++ test_recirculation_mux.py
@@ -0,0 +1,67 @@
+from cocotb_test.simulator import run
+import pytest
+import os
+
+vhdl_compile_args = "--std=08"
+sim_args = "--wave=wave.ghw"
+
+
+tests_dir = os.path.abspath(os.path.dirname(__file__)) #gives the path to the test(current) directory in which this test.py file is placed
+rtl_dir = tests_dir                                    #path to hdl folder where .vhdd files are placed
+
+
+      
+#run tests with generic values for length
+@pytest.mark.parametrize("parameter", [{"g_width": str(i)} for i in range(4,13,4)])
+def test_recirculation_mux(parameter):
+
+    module = "testbench"
+    toplevel = "recirculation_mux"   
+    vhdl_sources = [
+        os.path.join(rtl_dir, "toggle_synchronizer.vhd"),
+        os.path.join(rtl_dir, "recirculation_mux.vhd"),
+        ]
+
+
+
+    run(
+        python_search=[tests_dir],                         #where to search for all the python test files
+        vhdl_sources=vhdl_sources,
+        toplevel=toplevel,
+        module=module,
+
+        vhdl_compile_args=[vhdl_compile_args],
+        toplevel_lang="vhdl",
+        parameters=parameter,                              #parameter dictionary
+        extra_env=parameter,
+        sim_build="sim_build/"
+        + "_".join(("{}={}".format(*i) for i in parameter.items())),
+    )
+
+
+
+#run tests with generic values for length
+@pytest.mark.parametrize("parameter", [{"g_stages": str(i)} for i in range(2,5,1)])
+def test_toggle_synchronize(parameter):
+
+    module = "testbench_toggle"
+    toplevel = "toggle_synchronizer"   
+    vhdl_sources = [
+        os.path.join(rtl_dir, "toggle_synchronizer.vhd"),
+        ]
+
+
+
+    run(
+        python_search=[tests_dir],                         #where to search for all the python test files
+        vhdl_sources=vhdl_sources,
+        toplevel=toplevel,
+        module=module,
+
+        vhdl_compile_args=[vhdl_compile_args],
+        toplevel_lang="vhdl",
+        parameters=parameter,                              #parameter dictionary
+        extra_env=parameter,
+        sim_build="sim_build/"
+        + "_".join(("{}={}".format(*i) for i in parameter.items())),
+    )
